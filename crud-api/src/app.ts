c4eb42c483c45354bc2fd2ef595a714c33666acd
Dival Lucas <lusca404@gmail.com>
2025-04-16 07:19:29 -0300

    Organizing project, last commit

--- crud-api/src/app.ts
+++ crud-api/src/app.ts
@@ -3,20 +3,15 @@ import userRoutes from './routes/userRoutes';
 import './database/init';
 import cors from 'cors';
 
-
 const app = express();
-const PORT = 3000;
+
 app.use(cors({
   origin: ['http://localhost:3001', 'http://localhost:3002'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type']
 }));
 
-app.use(cors());
-
 app.use(express.json());
 app.use(userRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+export default app;
